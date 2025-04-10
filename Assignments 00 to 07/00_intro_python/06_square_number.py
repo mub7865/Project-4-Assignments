e82c35151bab818ddddfb6b6f4cfee0f45b692a7
Muhammad Ubaid Ansari <muhammadubaidansari145@gmail.com>
2025-04-10 23:22:21 +0500

    first commit

---
+++ Assignments 00 to 07/00_intro_python/06_square_number.py
@@ -0,0 +1,7 @@
+# 06_square_number
+def main():
+    user_input = float(input('Enter your number: '))
+    print(str(user_input) + " squared is " + str(user_input ** 2)) 
+
+if __name__ == "__main__":    
+    main()
---
+++ Assignments 00 to 07/01_expressions/01_dicesimulator.py
@@ -0,0 +1,30 @@
+"""
+Program: dicesimulator
+----------------------
+Simulate rolling two dice, three times.  Prints
+the results of each die roll.  This program is used
+to show how variable scope works.
+"""
+
+
+import random
+
+NUM_SIDES = 6
+
+def dice_roll():
+    dei1 :int = random.randint(1, NUM_SIDES)
+    dei2 :int = random.randint(1, NUM_SIDES)
+    total: int= dei1 + dei2
+    print("Total of two dice:", total)
+
+def main():
+    dei1= 10
+    print("die1 in main() starts as:" + str(dei1))
+    dice_roll()
+    # dice_roll()
+    # dice_roll()
+
+    print("die1 in main() is now:" + str(dei1))
+
+if __name__ == "__main__":
+    main()
